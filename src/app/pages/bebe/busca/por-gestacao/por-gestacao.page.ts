59b0613c39790fd88b6b27c9e4d731e5af533c51
Jefferson Cardoso <jefferson.mcardoso@gmail.com>
2019-09-01 18:19:52 -0300

    Adicionando 'carregando' nas telas para melhorar a usabilidade

--- src/app/pages/bebe/busca/por-gestacao/por-gestacao.page.ts
+++ src/app/pages/bebe/busca/por-gestacao/por-gestacao.page.ts
@@ -4,6 +4,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { BuscaBebeService } from '../../../../services/busca/busca-bebe.service';
 import { BuscaGestacaoService } from '../../../../services/busca/busca-gestacao.service';
 import { BuscaMaeService } from '../../../../services/busca/busca-mae.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-por-gestacao',
@@ -14,16 +15,27 @@ export class PorGestacaoPage implements OnInit {
 
   public bebes: any = []
   public gestacao: Object = null
+  public carregando: boolean = false
 
   constructor(
     private route: ActivatedRoute,
     private router: Router,
     private location: Location,
     private maeServico: BuscaMaeService,
+    private toastController: ToastController
   ) { }
 
   ngOnInit() {
     
+  }
+
+  private async mostrarMensagem(message) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 3000,
+      position: 'middle'
+    });
+    toast.present();
   }
 
   ionViewWillEnter() {
@@ -34,11 +46,17 @@ export class PorGestacaoPage implements OnInit {
   }
 
   async carregarGestacao(id) {
+    this.carregando = true
     this.gestacao = await this.maeServico.buscarGestacaoPorId(id)
+    this.carregando = false
   }
 
   async carregarBebes(id) {
+    this.carregando = true
     this.bebes = await this.maeServico.buscarBebePorGestacao(id)
+    this.carregando = false
+    if(this.bebes.length == 0)
+      this.mostrarMensagem("Nenhum bebê cadastrado")
   }
 
   voltar() {
--- src/app/pages/mae/consulta/busca/busca.page.ts
+++ src/app/pages/mae/consulta/busca/busca.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { BuscaMaeService } from '../../../../services/busca/busca-mae.service';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-busca',
@@ -13,8 +14,22 @@ export class BuscaPage implements OnInit {
   public busca: String = ""
   public maes: any = []
   private infiniteScroll = null
+  private carregando: boolean = false
 
-  constructor(private router: Router, private servico: BuscaMaeService) { }
+  constructor(
+    private router: Router, 
+    private servico: BuscaMaeService,
+    private toastController: ToastController
+  ) { }
+
+  private async mostrarMensagem(message) {
+    const toast = await this.toastController.create({
+      message: message,
+      duration: 3000,
+      position: 'bottom'
+    });
+    toast.present();
+  }
 
   voltar() {
     this.router.navigateByUrl("/inicio")
@@ -34,7 +49,14 @@ export class BuscaPage implements OnInit {
 
 
   async buscarMaes() {
+    this.carregando = true
     this.maes = await this.servico.buscar(this.busca)
+    this.carregando = false
+
+    if(this.maes.length == 0) {
+      this.mostrarMensagem("A busca não retornou resultado")
+    }
+
     this.pagina = 1
 
     if(this.infiniteScroll) 
@@ -45,7 +67,9 @@ export class BuscaPage implements OnInit {
     this.infiniteScroll = event.target
 
     this.pagina++
+    this.carregando = true
     let resultado = await this.servico.buscar(this.busca, this.pagina)
+    this.carregando = false
     
     if(resultado.length == 0) {
       this.infiniteScroll.disabled = true;
